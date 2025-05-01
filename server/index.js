6c30bd93fb934f176cceb87854fe9b5157047e5b
Mohd Rizwan <mohdrizwan962107@gmail.com>
2025-05-01 13:05:27 +0530

    E-learning LMS

--- server/index.js
+++ server/index.js
@@ -8,14 +8,15 @@ import courseRoute from "./routes/course.route.js";
 import mediaRoute from "./routes/media.route.js";
 import purchaseRoute from "./routes/purchaseCourse.route.js";
 import courseProgressRoute from "./routes/courseProgress.route.js";
+import path from "path";
 
 dotenv.config({});
-
-// call database connection here
+// connect to database
 connectDB();
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
 
 // default middleware
 app.use(express.json());
@@ -32,7 +33,11 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/purchase", purchaseRoute);
 app.use("/api/v1/progress", courseProgressRoute);
- 
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+ app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+ })
  
 app.listen(PORT, () => {
     console.log(`Server listen at port ${PORT}`);
